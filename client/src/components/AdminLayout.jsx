0053a552e3bff509e92999cd8a09180e4307049f
rohith-sunilkumar <rohith.sunilkumar09@gmail.com>
2026-03-19 01:55:11 +0530

    Updated Sidebar

--- client/src/components/AdminLayout.jsx
+++ client/src/components/AdminLayout.jsx
@@ -3,9 +3,9 @@ import AdminSidebar from "../components/admin/AdminSidebar.jsx";
 
 const AdminLayout = () => {
   return (
-    <div>
+    <div className="flex">
       <AdminSidebar />
-      <div className="ml-64 p-6 bg-gray-100 min-h-screen">
+      <div className="flex-1 md:ml-64 p-6 bg-gray-100 min-h-screen">
         <Outlet />
       </div>
     </div>
